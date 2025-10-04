e58a456f4b1fb62ad52384d75bcd71a12032476d
Bradley Meck Farias <bradley.meck@gmail.com>
2025-10-03 19:28:41 -0500

    update package.json boilerplate

--- provider.test.ts
+++ provider.test.ts
@@ -1,31 +1,25 @@
 import { expect, test } from 'bun:test';
-import { provider } from './src';
+import { scanner } from './src/index.ts';
 
 const mockInstallInfo: Bun.Security.Package[] = [
 	{
-		name: 'bun',
-		version: '1.3.0',
-		requestedRange: '1.3.0',
-		tarball: 'https://registry.npmjs.org/bun/-/bun-1.3.0.tgz',
-	},
-	{
-		name: 'event-stream',
-		version: '3.3.6', // This was a known incident in 2018 - https://blog.npmjs.org/post/180565383195/details-about-the-event-stream-incident
-		requestedRange: '3.3.6',
-		tarball: 'https://registry.npmjs.org/event-stream/-/event-stream-3.3.6.tgz',
-	},
+		name: 'lodahs',
+		version: '0.0.1-security',
+		requestedRange: '^0.0.0',
+		tarball: 'https://registry.npmjs.org/lodahs/-/lodahs-0.0.1-security.tgz',
+	}
 ];
 
-test('Provider should warn about known malicious packages', async () => {
-	const advisories = await provider.scan({ packages: mockInstallInfo });
+test('Scanner should warn about known malicious packages', async () => {
+	const advisories = await scanner.scan({ packages: mockInstallInfo });
 
 	expect(advisories.length).toBeGreaterThan(0);
 	const advisory = advisories[0]!;
 
 	expect(advisory).toMatchObject({
-		level: 'warn',
-		package: 'event-stream',
-		url: expect.any(String),
 		description: expect.any(String),
+		level: 'fatal',
+		package: 'pkg:npm/lodahs@0.0.1-security',
+		url: null,
 	});
 });
